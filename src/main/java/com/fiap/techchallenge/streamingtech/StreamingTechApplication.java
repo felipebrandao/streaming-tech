93114e6b54869d8330d7e8403f49016d0b1bcc95
Felipe Brandão <felipe.b31@gmail.com>
2024-01-28 20:59:57 -0300

    Primeiro Commit

---
+++ src/main/java/com/fiap/techchallenge/streamingtech/StreamingTechApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.streamingtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/techchallenge/streamingtech/StreamingTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.streamingtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
