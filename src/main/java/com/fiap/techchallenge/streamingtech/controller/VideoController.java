79b6ac3968e07bc115dd59bf974b1972e758808e
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2024-01-29 23:43:37 -0300

    import

--- src/main/java/com/fiap/techchallenge/streamingtech/controller/VideoController.java
+++ src/main/java/com/fiap/techchallenge/streamingtech/controller/VideoController.java
@@ -4,7 +4,15 @@ import com.fiap.techchallenge.streamingtech.model.Video;
 import com.fiap.techchallenge.streamingtech.model.VideoStats;
 import com.fiap.techchallenge.streamingtech.service.VideoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
