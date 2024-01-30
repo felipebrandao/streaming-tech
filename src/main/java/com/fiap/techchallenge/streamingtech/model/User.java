010d362b8069de0e5d1243106abdb6d122bac8f3
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2024-01-29 23:44:56 -0300

    Foi usado o arrayList na variavel favoriteVideos

--- src/main/java/com/fiap/techchallenge/streamingtech/model/User.java
+++ src/main/java/com/fiap/techchallenge/streamingtech/model/User.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Document
@@ -17,7 +18,7 @@ public class User {
 
     private String username;
     private String password;
-    private List<Video> favoriteVideos;
+    private List<Video> favoriteVideos = new ArrayList<>();
 
     public User(String username, String password) {
         this.username = username;
